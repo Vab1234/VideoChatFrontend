2b618fe9f5ed529db6f7e82bc8b0b0a1810cdfda
VarunBudhani <2022.varun.budhani@ves.ac.in>
2025-04-24 21:23:52 +0530

    fixes

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://16.171.12.126:3000"), []);
+  const socket = useMemo(() => io("https://16.171.92.33"), []);
 
   return (
     <SocketContext.Provider value={socket}>
